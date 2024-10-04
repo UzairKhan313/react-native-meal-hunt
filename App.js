a38d01ebd079bef1bc07168a4b46e91b37eabb66
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-10-04 16:40:33 +0500

    Adding font, svgs, image and spacer

--- App.js
+++ App.js
@@ -4,8 +4,28 @@ import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/theme";
 // Importing screens
 import RestaurantsScreen from "./src/screens/Restaurants";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded) {
+    return;
+  }
+
+  if (!latoLoaded) {
+    return;
+  }
   return (
     <>
       <ExpoStatusBar style="auto" />
