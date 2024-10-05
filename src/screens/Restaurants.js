0c4ab75810727ece9eb00ccba2f5c36dbdf40c9e
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-10-05 12:35:03 +0500

    Displying restaurant list

--- src/screens/Restaurants.js
+++ src/screens/Restaurants.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, SafeAreaView, StatusBar } from "react-native";
+import { View, SafeAreaView, StatusBar, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 
 import styled from "styled-components/native";
 
 import RestaurantCard from "../components/restaurants/Restaurant-Card";
+import { Spacer } from "../components/spacer/space";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -15,20 +16,22 @@ const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-  flex: 1;
-`;
-
 const RestaurantsScreen = () => {
   return (
     <SafeArea>
       <SearchContainer>
-        <Searchbar placeholder="Search Restaurants" />
+        <Searchbar placeholder="Search Restaurant" />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantCard />
-      </RestaurantListContainer>
+      <FlatList
+        data={[{ name: 1 }, { name: 2 }, { name: 3 }]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
